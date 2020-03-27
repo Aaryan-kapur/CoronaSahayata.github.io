759c01cf4ab9f0dcf51ba23446e485c2024dd59c
aaryan-kapur <ak6392@bennett.edu.in>
2020-03-27 19:36:13 +0530

    Updates

---
+++ precache-manifest.0c64eee6d55f6819b81921ec892190f2.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b502cd8c0648cf479b8de657caf6b1f",
+    "url": "/CoronaSahayata.github.io/./index.html"
+  },
+  {
+    "revision": "488d94f8b7e73f495737",
+    "url": "/CoronaSahayata.github.io/./static/css/2.e97aea52.chunk.css"
+  },
+  {
+    "revision": "440752fa7f37dd6442d4",
+    "url": "/CoronaSahayata.github.io/./static/css/main.2c9bfd34.chunk.css"
+  },
+  {
+    "revision": "488d94f8b7e73f495737",
+    "url": "/CoronaSahayata.github.io/./static/js/2.c8427b53.chunk.js"
+  },
+  {
+    "revision": "440752fa7f37dd6442d4",
+    "url": "/CoronaSahayata.github.io/./static/js/main.6d777392.chunk.js"
+  },
+  {
+    "revision": "8d5c56719238ccc740f8",
+    "url": "/CoronaSahayata.github.io/./static/js/runtime~main.50afcb71.js"
+  },
+  {
+    "revision": "6a6463ec608b526c328466466800b16f",
+    "url": "/CoronaSahayata.github.io/./static/media/Information1.6a6463ec.png"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/CoronaSahayata.github.io/./static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "3532fbc70300e04de6bbca8d0e84c222",
+    "url": "/CoronaSahayata.github.io/./static/media/Upper.3532fbc7.png"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/CoronaSahayata.github.io/./static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/CoronaSahayata.github.io/./static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "2a8a8a0d0cebfb91e92f983fd71f21b1",
+    "url": "/CoronaSahayata.github.io/./static/media/bg.2a8a8a0d.png"
+  },
+  {
+    "revision": "c42bad5630429c373b7465589c9f0401",
+    "url": "/CoronaSahayata.github.io/./static/media/bg1.c42bad56.png"
+  },
+  {
+    "revision": "43f8b431f1d01c4c44f2b7672f42d845",
+    "url": "/CoronaSahayata.github.io/./static/media/stats.43f8b431.png"
+  }
+]);
